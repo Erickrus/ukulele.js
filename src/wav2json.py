c1a1cec7ca49741f416a5da12126d25de0d3a881
Erickrus <hyinghao@hotmail.com>
2019-02-18 09:43:38 +0800

    handling the suffix for various type conversion

--- src/wav2json.py
+++ src/wav2json.py
@@ -3,11 +3,14 @@ import sys
 import json
 
 def wav2json(filename):
+    suffix = filename[-3:].lower()
     with open(filename, "rb") as wav_file:
-        encodedStr = u"data:audio/wav;base64," + \
+        encodedStr = (u"data:audio/%s;base64," % suffix) + \
             base64.b64encode(wav_file.read()).decode('utf-8')
         return {"base64wav":encodedStr, "filename":filename}
 
+
 jWav = wav2json(sys.argv[1])
-print("waves['%s'] = '%s';\n" % (jWav["filename"].lower().replace(".wav",""), jWav["base64wav"]))
+suffix = sys.argv[1][-3:].lower()
+print("waves['%s'] = '%s';\n" % (jWav["filename"].lower().replace("."+suffix,""), jWav["base64wav"]))
 
